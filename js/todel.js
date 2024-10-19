9f1b9e72862e19713ddaec1a6ee24ec0cd5bf7bd
Saja Hammad <128910907+Sajabh@users.noreply.github.com>
2024-10-19 22:13:29 +0300

    Delete js/todel.js

--- js/todel.js
+++
@@ -1,6 +0,0 @@
-let x = []
-x.push('saja hammad')
-x.push('leen not completed]')
-console.log(x[1].slice(-14))
-x[1] = 'saja' + (x[1].slice(-14)=='not completed]'? ' not completed':' completed');
-console.log(x)
